3ffcab79ef245e315b8e977e3f92e7b440e2bec2
Snehal <nanavaresnehal115@gmail.com>
2022-08-10 11:23:38 +0530

    ADDED[Snehal]- UC-7 Class Method to Compute Employee Wage

--- src/EmployeeWage/BridgeLabz/Employee_Wage.java
+++ src/EmployeeWage/BridgeLabz/Employee_Wage.java
@@ -1,61 +1,69 @@
 package EmployeeWage.BridgeLabz;
 /*
- * *********Added UC6*********
- * Calculate Wages till a condition of total working hours or 
-   days is reached for a month - Assume 100 hours
+ *UC7 - Refactor the Code to write a Class Method to Compute Employee Wage -
  */
 
-public class Employee_Wage 
+public class Employee_Wage
 {
-	public static void main(String a[])
+	//Constants
+	public static final int is_part_time = 1;
+	public static final int is_full_time = 2;
+	public static final int emp_rate_perhr = 10;
+	public static final int num_of_working_days = 20;
+	public static final int max_hrs_in_month = 100;
+	
+	//Class variables
+	//Variables 
+	 int totalWorkingDays = 0;
+	 int totalEmpHrs = 0;
+	 int totalEmpWage=0;
+	
+	public int Compute_EmployeeWage()
 	{
-		//Constants
-		final int is_part_time = 1;
-		final int is_full_time = 2;
-		final int emp_rate_perhr = 20;
-		final int num_of_working_days = 20;
-		final int max_hrs_in_month = 100;
 		
-		//Variables 
-		 int totalWorkingDays = 0;
-		 int totalEmpHrs = 0;
-		 int totalempwage = 0;
-		 
 		
-		while(totalEmpHrs <= max_hrs_in_month  &&  totalWorkingDays <= num_of_working_days)
+		while(totalEmpHrs <= max_hrs_in_month && totalWorkingDays <= num_of_working_days)
 		 {	
-			 int emphrs = 0;
-			 int empwage = 0;
+			 int empHrs = 0;
+			 int empWage = 0;
 			
-			 totalWorkingDays++; 
-			 
-			 int emp_check = (int) Math.floor(Math.random() * 10 ) % 3;
-			 
-			 switch(emp_check)
+			 totalWorkingDays++;
+			 int emp_Check = (int) Math.floor ( Math.random() * 10 ) % 3;
+			
+			 switch(emp_Check)
 			 {
-			 case is_part_time : 
-				 System.out.println( "Part Time");
-				 emphrs = 4;	
-				 break;
-				 
-			 case is_full_time :
-				 System.out.println( "Employee is Present....");
-				 emphrs = 8;
-				 break;
-				 
-			 default :
-				 System.out.println("Employee is Abscent.... ");
-				 emphrs = 0; 
-				 break;
+			 	case is_full_time:
+			 		empHrs = 8;
+			 		break;
+			 		
+			 	case is_part_time: 
+			 		empHrs = 4;
+			 		break;
+			 		
+			 	default:
+			 		empHrs = 0; 
+			 		break;
+			 	 
 			 }
-			 totalEmpHrs = emphrs;
-			 empwage = emphrs * emp_rate_perhr;
-			 totalempwage += empwage;
-			 System.out.println("Employee Wage =" +empwage);
-
+			 
+			 totalEmpHrs = empHrs;
+			 empWage=empHrs * emp_rate_perhr;
+			 totalEmpWage += empWage;
+			 System.out.println("Employee Wage =" +empWage);	 
+		 
 		 }
-		 System.out.println("\nTotal Employee Hours =" +totalEmpHrs);
-		 System.out.println("\nTotal Employee Wage =" +totalempwage);		
+		
+		System.out.println("\nTotal Employee Hours =" +totalEmpHrs);
+		
+		return totalEmpWage;
 	}
-
+			
+	public static void main(String args[])
+	{
+		Employee_Wage  Emp = new Employee_Wage();
+		
+		int result = Emp.Compute_EmployeeWage();
+		System.out.println("Total Employee Wage =" +result);
+	}				
 }
+
