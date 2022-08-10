4d990df622b1be86bc62b4650dfdab86f9bfd25e
Snehal <nanavaresnehal115@gmail.com>
2022-08-10 11:19:34 +0530

    ADDED[Snehal]- UC-6 Calculate Wages till a condition

--- src/EmployeeWage/BridgeLabz/Employee_Wage.java
+++ src/EmployeeWage/BridgeLabz/Employee_Wage.java
@@ -1,7 +1,8 @@
 package EmployeeWage.BridgeLabz;
 /*
- * *********Added UC5*********
- * Calculating Employee Wages for a Month
+ * *********Added UC6*********
+ * Calculate Wages till a condition of total working hours or 
+   days is reached for a month - Assume 100 hours
  */
 
 public class Employee_Wage 
@@ -13,27 +14,33 @@ public class Employee_Wage
 		final int is_full_time = 2;
 		final int emp_rate_perhr = 20;
 		final int num_of_working_days = 20;
+		final int max_hrs_in_month = 100;
 		
 		//Variables 
-		 int emphrs = 0;
-		 int empwage = 0;
+		 int totalWorkingDays = 0;
+		 int totalEmpHrs = 0;
 		 int totalempwage = 0;
 		 
 		
-		
-		 for(int day = 0; day < num_of_working_days; day++) 
-		 {
+		while(totalEmpHrs <= max_hrs_in_month  &&  totalWorkingDays <= num_of_working_days)
+		 {	
+			 int emphrs = 0;
+			 int empwage = 0;
+			
+			 totalWorkingDays++; 
+			 
 			 int emp_check = (int) Math.floor(Math.random() * 10 ) % 3;
+			 
 			 switch(emp_check)
 			 {
-			 case is_full_time :
-				 System.out.println( "Employee is Present....");
-				 emphrs = 8;
-				 break;
-				 
 			 case is_part_time : 
 				 System.out.println( "Part Time");
 				 emphrs = 4;	
+				 break;
+				 
+			 case is_full_time :
+				 System.out.println( "Employee is Present....");
+				 emphrs = 8;
 				 break;
 				 
 			 default :
@@ -41,13 +48,14 @@ public class Employee_Wage
 				 emphrs = 0; 
 				 break;
 			 }
-			 empwage=emphrs * emp_rate_perhr;
+			 totalEmpHrs = emphrs;
+			 empwage = emphrs * emp_rate_perhr;
 			 totalempwage += empwage;
 			 System.out.println("Employee Wage =" +empwage);
 
 		 }
-		
-		 System.out.println("Total Employee Wage =" +totalempwage);		
+		 System.out.println("\nTotal Employee Hours =" +totalEmpHrs);
+		 System.out.println("\nTotal Employee Wage =" +totalempwage);		
 	}
 
 }
