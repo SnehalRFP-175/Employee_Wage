1eff6b72d97fcb856ec7d875ebc84d20d0328b77
Snehal <nanavaresnehal115@gmail.com>
2022-08-10 11:01:04 +0530

    ADDED[Snehal]- UC-2 Calculating Daily Employee Wage

--- src/EmployeeWage/BridgeLabz/Employee_Wage.java
+++ src/EmployeeWage/BridgeLabz/Employee_Wage.java
@@ -1,26 +1,34 @@
 package EmployeeWage.BridgeLabz;
 
 /*
- * *********Added UC1*********
- * Checking Employee is Present or Not
+ * *********Added UC2*********
+ * Calculating Daily Employee Wage
  */
+
 public class Employee_Wage 
 {
 	public static void main(String a[])
 	{
 		int is_full_time = 1;
+		int emp_rate_perhr = 20;
+		
+		 
+		 int emphrs = 0;
+		 int empwage = 0;
 		
 		double emp_check = Math.floor(Math.random() * 10 ) % 2;
 		
 		if(emp_check == is_full_time)
 		{
 			System.out.println( "Employee is Present....");
+			 emphrs = 8;
 		}
 		else
-		{
+			{
 			System.out.println("Employee is Abscent.... ");
-		}
-			
+			}
+			empwage = emphrs * emp_rate_perhr;
+		 System.out.println("Employee Wage =" +empwage);
 	}
 
 }
